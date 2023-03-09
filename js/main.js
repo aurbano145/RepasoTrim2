002d5f67b2e4031b130ba3764742e142b332df64
aurbano145 <80650797+aurbano145@users.noreply.github.com>
2023-03-09 20:09:03 +0100

    Delete main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import {Regex} from "./regex.js";
-
-var email = document.getElementById("email");
-var check = document.getElementById("check");
-var result = document.getElementById("result");
-
-var regex = new Regex();
-
-check.addEventListener("click", function() {regex.checkEmail(email.value, result)});
