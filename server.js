df28d7f11f989c280bb3adccf496f518cebd8f75
aurbano145 <80650797+aurbano145@users.noreply.github.com>
2023-03-09 20:09:42 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 var jsonParser = bodyParser.json();
+
+app.use(cors());
 
 app.post("/regex", jsonParser, function (req, res) {
 
